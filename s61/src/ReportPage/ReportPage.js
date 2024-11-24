c4384185be4ac2f68489b1e3919707c603c8fb35
Sakarmit <40968890+Sakarmit@users.noreply.github.com>
2024-11-23 22:25:37 -0500

    Both Charts Added

--- s61/src/ReportPage/ReportPage.js
+++ s61/src/ReportPage/ReportPage.js
@@ -1,6 +1,40 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { BarChart } from '@mui/x-charts/BarChart';
+
 function ReportPage() {
+    const [data, setData] = useState(null);
+
+    useEffect(() => {
+        const token = localStorage.getItem('token');
+        axios.get('http://localhost:3000/api/energyUse', {
+            headers: {
+            'Authorization': `Bearer ${token}`
+        }}).then(response => {
+            setData(response.data);
+        }).catch(error => {
+            console.error('There was an error fetching the data!', error);
+        });
+    }, []);
+
     return (
         <main>
+            {data && (
+                <BarChart
+                    width={500}
+                    height={500}
+                    series={[
+                        {
+                            data: data.map(item => item.totalEnergyUse),
+                            valueFormatter: (val) => {
+                                return `${val} TWh`;
+                            }
+                        }
+                    ]}
+                    xAxis={[{data: data.map(item => item.year), label: 'World Electricity Demand (TWh)', scaleType: 'band' }]}
+                    
+                />
+            )}
             <p>
                 Report Placeholder
             </p>
--- s61/src/SummaryPage/SummaryPage.js
+++ s61/src/SummaryPage/SummaryPage.js
@@ -1,6 +1,44 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { PieChart, pieArcLabelClasses } from '@mui/x-charts/PieChart';
+
 function SummaryPage() {
+    const [data, setData] = useState(null);
+
+    useEffect(() => {
+        const token = localStorage.getItem('token');
+        axios.get('http://localhost:3000/api/energySources', {
+            headers: {
+            'Authorization': `Bearer ${token}`
+        }}).then(response => {
+            setData(response.data);
+        }).catch(error => {
+            console.error('There was an error fetching the data!', error);
+        });
+    }, []);
+
     return (
         <main>
+            {data && (
+                <PieChart
+                    width={700}
+                    height={400}
+                    series={[
+                        {
+                            data: data.map((item, index) => {
+                                return {id: index, label: item.source, value: item.value};
+                            }),
+                            valueFormatter: (val) => {
+                                return `${val.value}%`;
+                            },
+                            borderColor: 'black',
+                            innerRadius: '50%',
+                            paddingAngle: 0.3,
+                            highlightScope: { fade: 'global', highlight: 'item' },
+                        }
+                    ]}
+                />
+            )}
             <p>
                 Summary Placeholder
             </p>
