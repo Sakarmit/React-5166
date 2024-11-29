e4397e654f8130cf5bf623ea28d334e412831a0e
Sakarmit <40968890+Sakarmit@users.noreply.github.com>
2024-11-29 18:46:16 -0500

    Home Page Summary

--- s61/src/HomePage/HomePage.js
+++ s61/src/HomePage/HomePage.js
@@ -1,11 +1,39 @@
 function HomePage() {
-    return (
-        <main>
-            <p>
-                Home Placeholder
-            </p>
-        </main>
-    );
-};
+  return (
+    <main>
+        <img src="solar-panels.svg" alt=""/>
+      <p>
+        Scientists at Oxford University have developed a groundbreaking
+        technology that could revolutionize solar energy. Their innovation
+        involves an ultra-thin, light-absorbing material that can be applied to
+        almost any surface, such as cars, and buildings. This new material uses
+        a multi-junction approach, stacking multiple light-absorbing layers to
+        harness a wider range of the light spectrum, resulting in higher power
+        generation from the same amount of sunlight.
+      </p>
+      <p>
+        The material has been independently certified by Japan's National
+        Institute of Advanced Industrial Science and Technology to deliver over
+        27% energy efficiency, matching the performance of traditional
+        silicon-based solar panels for the first time. The researchers believe
+        that this efficiency could eventually exceed 45%. The ultra-thin
+        material, just over one micron thick, is almost 150 times thinner than a
+        silicon wafer, making it highly versatile and flexible.
+      </p>
+      <p>
+        This breakthrough could significantly reduce the need for large-scale
+        silicon-based solar farms and lower the cost of solar energy. The
+        researchers envision a future where solar power can be generated from
+        everyday objects, making solar energy more accessible and sustainable.
+        The team, led by Professor Henry Snaith, has been at the forefront of
+        photovoltaic research for over a decade and has already established
+        Oxford PV, a company focused on commercializing this technology.
+        <a href="https://www.ox.ac.uk/news/2024-08-09-solar-energy-breakthrough-could-reduce-need-solar-farms">
+          [Source]
+        </a>
+      </p>
+    </main>
+  );
+}
 
 export default HomePage;
