c8202e6b5ef5d5b8141d70d01092f2bfd60b5d75
Sakarmit <40968890+Sakarmit@users.noreply.github.com>
2024-11-29 15:16:29 -0500

    Summary/Report Page Paragraphs

--- s61/src/ReportPage/ReportPage.js
+++ s61/src/ReportPage/ReportPage.js
@@ -1,45 +1,63 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import { BarChart } from '@mui/x-charts/BarChart';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { BarChart } from "@mui/x-charts/BarChart";
 
 function ReportPage() {
-    const [data, setData] = useState(null);
+  const [data, setData] = useState(null);
 
-    useEffect(() => {
-        const token = localStorage.getItem('token');
-        axios.get('http://localhost:3000/api/energyUse', {
-            headers: {
-            'Authorization': `Bearer ${token}`
-        }}).then(response => {
-            setData(response.data);
-        }).catch(error => {
-            console.error('There was an error fetching the data!', error);
-        });
-    }, []);
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    axios
+      .get("http://localhost:3000/api/energyUse", {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then((response) => {
+        setData(response.data);
+      })
+      .catch((error) => {
+        console.error("There was an error fetching the data!", error);
+      });
+  }, []);
 
-    return (
-        <main>
-            {data && (
-                <BarChart
-                    width={500}
-                    height={500}
-                    series={[
-                        {
-                            data: data.map(item => item.totalEnergyUse),
-                            valueFormatter: (val) => {
-                                return `${val} TWh`;
-                            }
-                        }
-                    ]}
-                    xAxis={[{data: data.map(item => item.year), label: 'World Electricity Demand (TWh)', scaleType: 'band' }]}
-                    
-                />
-            )}
-            <p>
-                Report Placeholder
-            </p>
-        </main>
-    );
-};
+  return (
+    <main>
+      {data && (
+        <BarChart
+          width={500}
+          height={500}
+          series={[
+            {
+              data: data.map((item) => item.totalEnergyUse),
+              valueFormatter: (val) => {
+                return `${val} TWh`;
+              },
+            },
+          ]}
+          xAxis={[
+            {
+              data: data.map((item) => item.year),
+              label: "World Electricity Demand (TWh)",
+              scaleType: "band",
+            },
+          ]}
+        />
+      )}
+      <p>
+        The chart illustrates the yearly world electricity demand from 2000 to
+        2022, measured in terawatt-hours (TWh). The data, sourced from
+        <a href="https://ember-energy.org/data/yearly-electricity-data/">
+          Ember's Yearly Electricity Data
+        </a>
+        , shows a consistent upward trend in global electricity consumption over
+        the 22-year period. This significant rise highlights the growing global
+        reliance on electricity. The chart underscores the importance of sustainable energy
+        solutions to meet the escalating demand while addressing environmental
+        concerns.
+      </p>
+    </main>
+  );
+}
 
 export default ReportPage;
--- s61/src/SummaryPage/SummaryPage.js
+++ s61/src/SummaryPage/SummaryPage.js
@@ -1,49 +1,62 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import { PieChart } from '@mui/x-charts/PieChart';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { PieChart } from "@mui/x-charts/PieChart";
 
 function SummaryPage() {
-    const [data, setData] = useState(null);
+  const [data, setData] = useState(null);
 
-    useEffect(() => {
-        const token = localStorage.getItem('token');
-        axios.get('http://localhost:3000/api/energySources', {
-            headers: {
-            'Authorization': `Bearer ${token}`
-        }}).then(response => {
-            setData(response.data);
-        }).catch(error => {
-            console.error('There was an error fetching the data!', error);
-        });
-    }, []);
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    axios
+      .get("http://localhost:3000/api/energySources", {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then((response) => {
+        setData(response.data);
+      })
+      .catch((error) => {
+        console.error("There was an error fetching the data!", error);
+      });
+  }, []);
 
-    return (
-        <main>
-            {data && (
-                <PieChart
-                    width={700}
-                    height={400}
-                    series={[
-                        {
-                            data: data.map((item, index) => {
-                                return {id: index, label: item.source, value: item.value};
-                            }),
-                            valueFormatter: (val) => {
-                                return `${val.value}%`;
-                            },
-                            borderColor: 'black',
-                            innerRadius: '50%',
-                            paddingAngle: 0.3,
-                            highlightScope: { fade: 'global', highlight: 'item' },
-                        }
-                    ]}
-                />
-            )}
-            <p>
-                Summary Placeholder
-            </p>
-        </main>
-    );
-};
+  return (
+    <main>
+      {data && (
+        <PieChart
+          width={700}
+          height={400}
+          series={[
+            {
+              data: data.map((item, index) => {
+                return { id: index, label: item.source, value: item.value };
+              }),
+              valueFormatter: (val) => {
+                return `${val.value}%`;
+              },
+              borderColor: "black",
+              innerRadius: "50%",
+              paddingAngle: 0.3,
+              highlightScope: { fade: "global", highlight: "item" },
+            },
+          ]}
+        />
+      )}
+      <p>
+        The data from
+        <a href="https://ourworldindata.org/grapher/share-elec-by-source?time=latest&v=1&csvType=filtered&useColumnShortNames=false">
+          &nbsp;Our World in Data
+        </a>
+        &nbsp;highlights the sources of energy used for electricity generation
+        worldwide. Coal remains the largest contributor at 35.51%, followed by
+        gas at 22.47% and hydro at 14.28%. Nuclear energy accounts for 9.11% of
+        the total, while wind and solar contribute 7.82% and 5.53%,
+        respectively. Oil and bioenergy have smaller shares, at 2.67% and 2.30%,
+        with other renewables making up the smallest portion at 0.30%.
+      </p>
+    </main>
+  );
+}
 
 export default SummaryPage;
