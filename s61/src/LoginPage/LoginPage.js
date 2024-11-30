ac4b83f11c13221c51e805745de1c532e18b9d3f
Sakarmit <40968890+Sakarmit@users.noreply.github.com>
2024-11-29 22:11:32 -0500

    Login Page CSS

--- s61/src/LoginPage/LoginPage.js
+++ s61/src/LoginPage/LoginPage.js
@@ -1,60 +1,58 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { loginUser } from '../Helpers/userManager';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { loginUser } from "../Helpers/userManager";
 
 function LoginPage(props) {
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
-    const [error, setError] = useState('');
-    const navigate = useNavigate();
-    
-    const handleSubmit = async (event) => {
-        event.preventDefault();
-        const error = await loginUser(username, password);
-        if (error) {
-            setError(error);
-        } else {
-            props.setIsLoggedIn(true);
-        }
-    };
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
 
-    if (props.isLoggedIn) {
-        navigate('/');
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const error = await loginUser(username, password);
+    if (error) {
+      setError(error);
+    } else {
+      props.setIsLoggedIn(true);
     }
+  };
 
-    return (
-        <main>
-            <h2>
-                Login to access site
-            </h2>
-            <form onSubmit={handleSubmit}>
-                <div>
-                    <label htmlFor="username">Username:</label>
-                    <input
-                        type="text"
-                        id="username"
-                        name="username"
-                        value={username}
-                        onChange={(e) => setUsername(e.target.value)}
-                        required
-                    />
-                </div>
-                <div>
-                    <label htmlFor="password">Password:</label>
-                    <input
-                        type="password"
-                        id="password"
-                        name="password"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                        required
-                    />
-                </div>
-                {error && <p>{error}</p>}
-                <button type="submit">Login</button>
-            </form>
-        </main>
-    );
+  if (props.isLoggedIn) {
+    navigate("/");
+  }
+
+  return (
+    <main>
+      <form onSubmit={handleSubmit}>
+        <h2>Login to access site</h2>
+        <div>
+          <label htmlFor="username">Username:</label>
+          <input
+            type="text"
+            id="username"
+            name="username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+        {error && <p>{error}</p>}
+        <button type="submit">Login</button>
+      </form>
+    </main>
+  );
 }
 
 export default LoginPage;
